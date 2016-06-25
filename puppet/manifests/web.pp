29cb82af060270b4c8c8a5a059ecf98ecf6412a1
Andrew Jackson <jackson.andrew@gmail.com>
2016-06-25 11:33:11 +0800

    Added PHP packages and test page to puppet manifest

--- puppet/manifests/web.pp
+++ puppet/manifests/web.pp
@@ -12,3 +12,23 @@ package { 'apache2':
 service { 'apache2':
   ensure => running,
 }
+
+# install PHP and Mysql module
+package { 'php5':
+  ensure => installed,
+  require => [ Exec['apt-update'], Package['apache2'] ],
+}
+
+# install Mysql module for PHP
+package { 'php5-mysql':
+  ensure => installed,
+  require => [ Exec['apt-update'], Package['apache2'] ],
+  notify => Service['apache2'],
+}
+
+# PHP Info page, to ensure PHP is installed correctly
+file { '/var/www/html/info.php':
+  ensure => file,
+  content => '<?php  phpinfo(); ?>',    # phpinfo code
+  require => Package['php5'],
+}
